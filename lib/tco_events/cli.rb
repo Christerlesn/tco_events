651af4fa2822ab22c2f935babfa83da756b59d51
Christerle Nounoute <christerlesn@gmail.com>
2020-11-18 23:11:51 +0000

    updated cli.rb

--- lib/tco_events/cli.rb
+++ lib/tco_events/cli.rb
@@ -2,63 +2,169 @@ class TcoEvents::CLI
   
   def call
     puts "Welcome to Tuolumne County Events Page!"
-    months
+    # months
     month_events
     goodbye
   end
   
-  def months
-    puts <<-DOC 
-    01. Januaray
-    02. Feburary
-    03. March
-    04. April
-    05. May 
-    06. June 
-    07. July
-    08. August 
-    09. September 
-    10. October 
-    11. November 
-    12. December
-    DOC
-  end
+  # def months
+  #   puts <<-DOC 
+  #   01. Januaray
+  #   02. Feburary
+  #   03. March
+  #   04. April
+  #   05. May 
+  #   06. June 
+  #   07. July
+  #   08. August 
+  #   09. September 
+  #   10. October 
+  #   11. November 
+  #   12. December
+  #   DOC
+  # end
   
-  def month_events
-    user_input = nil 
-    while user_input != 'exit'
-      puts "Type in the month you would like to see events for in mm format(i.e: 01), or type exit:"
-      user_input = gets.strip.downcase
+    def month_events
+      user_input = nil
+      while user_input != "exit"
+        puts "Type in the month you would like to see events for in mm format(i.e: 01), or type exit:"
+        user_input = gets.strip.downcase
       case user_input
-        when "01"
-          puts "You have selected January!"
-        when "02"
-          puts "You have selected Feburary!"
+      when "01"
+        puts "These are all the events for January:
+        1. event 1
+        2. event 2"
+        day
+      when "02"
+        puts "These are all the events for Feburary:
+        1. event 1
+        2. event 2"
+        day
       when "03"
-          puts "You have selected March!"
+        puts "These are all the events for March:
+        1. event 1
+        2. event 2"
+        day
       when "04"
-          puts "You have selected April!"
+        puts "These are all the events for April:
+        1. event 1
+        2. event 2"
+        day
       when "05"
-          puts "You have selected May!"
+        puts "These are all the events for May:
+        1. event 1
+        2. event 2"
+        day
       when "06"
-          puts "You have selected June!"
+        puts "These are all the events for June:
+        1. event 1
+        2. event 2"
+        day
       when "07"
-          puts "You have selected July!"
+        puts "These are all the events for July:
+        1. event 1
+        2. event 2"
+        day
       when "08"
-          puts "You have selected August!"
+        puts "These are all the events for August:
+        1. event 1
+        2. event 2"
+        day
       when "09"
-          puts "You have selected September!"
+        puts "These are all the events for September:
+        1. event 1
+        2. event 2"
+        day
       when "10"
-          puts "You have selected October!"
+        puts "These are all the events for October:
+        1. event 1
+        2. event 2"
+        day
       when "11"
-          puts "You have selected November!"
+        puts "These are all the events for November:
+        1. event 1
+        2. event 2"
+        day
       when "12"
-          puts "You have selected December!"
-      else
-        puts "Invalid input. Please try again:"
-        end
+        puts "These are all the events for December:
+        1. event 1
+        2. event 2"
+        day
       end
     end
+  end
+    
+    def day
+      input = nil 
+      while input != "exit"
+        puts "Type in the day you would like to see events for in dd format(i.e: 01), or type exit to return to month menu:"
+        input = gets.strip.downcase
+        case input
+            when "01"
+              puts "1st"
+            when "02"
+              puts "2nd"
+            when "03"
+              puts "3rd"
+            when "04"
+              puts "4th"
+            when "05"
+              puts "5th"
+            when "06"
+              puts "6th"
+            when "07"
+              puts "7th"
+            when "08"
+              puts "8th"
+            when "09"
+              puts "9th"
+            when "10"
+              puts "10th"
+            when "11"
+              puts "11th"
+            when "12"
+              puts "12th"
+            when "13"
+              puts "13th"
+            when "14"
+              puts "14th"
+            when "15"
+              puts "15th"
+            when "16"
+              puts "16th"
+            when "17"
+              puts "17th"
+            when "18"
+              puts "18th"
+            when "19"
+              puts "19th"
+            when "20"
+              puts "20th"
+            when "21"
+              puts "21st"
+            when "22"
+              puts "22nd"
+            when "23"
+              puts "23rd"
+            when "24"
+              puts "24th"
+            when "25"
+              puts "25th"
+            when "26"
+              puts "26th"
+            when "27"
+              puts "27th"
+            when "28"
+              puts "28th"
+            when "29"
+              puts "29th"
+            when "30"
+              puts "30th"
+            when "31"
+              puts "31st"
+            end
+          end
+        end
     
     def goodbye
       puts "Thanks for visiting, come back soon!"
