6b83eb841f337a0e348eaa3273a73f8e1794d0f9
Christerle Nounoute <christerlesn@gmail.com>
2020-11-25 23:09:15 +0000

    started modifying day_event

--- lib/tco_events/cli.rb
+++ lib/tco_events/cli.rb
@@ -2,13 +2,13 @@ class TcoEvents::CLI
   
   def call
     puts "Welcome to Tuolumne County Events Page!"
-    list_events
+    @events = TcoEvents::Event.annual
     month_events
     goodbye
   end
   
   def list_events
-    @events = TcoEvents::Event.annual
+    #@events = TcoEvents::Event.annual
     @events.each.with_index(1) do |event, i|
       puts "#{i}. #{event.month}: #{event.info}. For more information, go to #{event.url}"
     end
@@ -23,7 +23,6 @@ class TcoEvents::CLI
         if input.to_i > 0
           m_event = @events[input.to_i-1]
           puts "#{m_event.month}: #{m_event.info}. For more information, go to #{m_event.url}"
-          day
         elsif input == "annual events"
           list_events
         else
@@ -31,79 +30,10 @@ class TcoEvents::CLI
         end
       end
     end
-          
     
-    def day
-      input = nil 
-      while input != "exit"
-        puts "Type in the day you would like to see events for in dd format(i.e: 01), ranging from 01-31, or type exit to return to the main menu:"
-        input = gets.strip.downcase
-        case input
-            when "01"
-              puts "1st"
-            when "02"
-              puts "2nd"
-            when "03"
-              puts "3rd"
-            when "04"
-              puts "4th"
-            when "05"
-              puts "5th"
-            when "06"
-              puts "6th"
-            when "07"
-              puts "7th"
-            when "08"
-              puts "8th"
-            when "09"
-              puts "9th"
-            when "10"
-              puts "10th"
-            when "11"
-              puts "11th"
-            when "12"
-              puts "12th"
-            when "13"
-              puts "13th"
-            when "14"
-              puts "14th"
-            when "15"
-              puts "15th"
-            when "16"
-              puts "16th"
-            when "17"
-              puts "17th"
-            when "18"
-              puts "18th"
-            when "19"
-              puts "19th"
-            when "20"
-              puts "20th"
-            when "21"
-              puts "21st"
-            when "22"
-              puts "22nd"
-            when "23"
-              puts "23rd"
-            when "24"
-              puts "24th"
-            when "25"
-              puts "25th"
-            when "26"
-              puts "26th"
-            when "27"
-              puts "27th"
-            when "28"
-              puts "28th"
-            when "29"
-              puts "29th"
-            when "30"
-              puts "30th"
-            when "31"
-              puts "31st"
-            end
-          end
-        end
+    def day_event
+      puts "testing for now"
+    end
     
     def goodbye
       puts "Thanks for visiting, come back soon!"
--- lib/tco_events/event.rb
+++ lib/tco_events/event.rb
@@ -2,59 +2,7 @@ class TcoEvents::Event
   attr_accessor :month, :info, :url
 
   def self.annual
-    #shows all the events for the year
-    # puts <<-DOC 
-    # These are all the events for the year
-    # 01. January
-    #   These are all the events for January:
-    #     1. event 1
-    #     2. event 2
-    # 02. Feburary
-    #   These are all the events for Feburary:
-    #     1. event 1
-    #     2. event 2
-    # 03. March
-    #   These are all the events for March:
-    #     1. event 1
-    #     2. event 2
-    # 04. April
-    #   These are all the events for April:
-    #     1. event 1
-    #     2. event 2
-    # 05. May
-    #   These are all the events for May:
-    #     1. event 1
-    #     2. event 2
-    # 06. June
-    #   These are all the events for June:
-    #     1. event 1
-    #     2. event 2
-    # 07. July
-    #   These are all the events for July:
-    #     1. event 1
-    #     2. event 2
-    # 08. August
-    #   These are all the events for August:
-    #     1. event 1
-    #     2. event 2
-    # 09. September
-    #   These are all the events for September:
-    #     1. event 1
-    #     2. event 2
-    # 10. October
-    #   These are all the events for October:
-    #     1. event 1
-    #     2. event 2
-    # 11. November
-    #   These are all the events for November:
-    #     1. event 1
-    #     2. event 2
-    # 12. December
-    #   These are all the events for December:
-    #     1. event 1
-    #     2. event 2
-    # DOC
-    
+
     event_1 = self.new
     event_1.month = "January"
     event_1.info = "These are all the events for January:
@@ -142,6 +90,77 @@ class TcoEvents::Event
     [event_1, event_2, event_3, event_4, event_5, event_6, event_7, event_8, event_9, event_10, event_11, event_12]
   end
 
-
+   def day
+      #@day_event = TcoEvents::Event.day_chosen
+      input = nil 
+      while input != "exit"
+        puts "Type in the day you would like to see events for in dd format(i.e: 01), ranging from 01-31, or type exit to return to the main menu:"
+        input = gets.strip.downcase
+        case input
+            when "01"
+              puts "1st"
+            when "02"
+              puts "2nd"
+            when "03"
+              puts "3rd"
+            when "04"
+              puts "4th"
+            when "05"
+              puts "5th"
+            when "06"
+              puts "6th"
+            when "07"
+              puts "7th"
+            when "08"
+              puts "8th"
+            when "09"
+              puts "9th"
+            when "10"
+              puts "10th"
+            when "11"
+              puts "11th"
+            when "12"
+              puts "12th"
+            when "13"
+              puts "13th"
+            when "14"
+              puts "14th"
+            when "15"
+              puts "15th"
+            when "16"
+              puts "16th"
+            when "17"
+              puts "17th"
+            when "18"
+              puts "18th"
+            when "19"
+              puts "19th"
+            when "20"
+              puts "20th"
+            when "21"
+              puts "21st"
+            when "22"
+              puts "22nd"
+            when "23"
+              puts "23rd"
+            when "24"
+              puts "24th"
+            when "25"
+              puts "25th"
+            when "26"
+              puts "26th"
+            when "27"
+              puts "27th"
+            when "28"
+              puts "28th"
+            when "29"
+              puts "29th"
+            when "30"
+              puts "30th"
+            when "31"
+              puts "31st"
+            end
+          end
+        end
 
 end
